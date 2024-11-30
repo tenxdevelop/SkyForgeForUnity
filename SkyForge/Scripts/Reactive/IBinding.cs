a91205cd1fb8195138c58269c35e734f8702407d
tenxdeveloper <tenxdeveloper@bk.ru>
2024-11-30 21:46:29 +0500

    Added Reactive version 1.0.3

---
+++ SkyForge/Scripts/Reactive/IBinding.cs
@@ -0,0 +1,13 @@
+﻿/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using System;
+
+namespace SkyForge.Reactive
+{
+    public interface IBinding : IDisposable
+    {
+        void Binded();
+    }
+}
---
+++ SkyForge/Scripts/Reactive/IReactiveProperty.cs
@@ -0,0 +1,11 @@
+﻿/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+namespace SkyForge.Reactive
+{
+    public interface IReactiveProperty<out T> : IObservable<T>
+    {
+        T Value { get; }  
+    }
+}
