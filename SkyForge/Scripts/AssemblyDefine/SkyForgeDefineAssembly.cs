2d3632f1f184bf6dac4b98b7d8dedaf73735b6dc
tenxdevelop <tenxdeveloper@bk.ru>
2025-07-22 13:35:02 +0500

    Remove custom assembly

--- SkyForge/Scripts/AssemblyDefine/SkyForgeDefineAssembly.cs
+++ SkyForge/Scripts/AssemblyDefine/SkyForgeDefineAssembly.cs
@@ -3,9 +3,6 @@
 \**************************************************************************/
 
 using System.Reflection;
-using UnityEditor;
-using UnityEngine;
-using System.IO;
 using System;
 
 namespace SkyForge
@@ -13,43 +10,13 @@ namespace SkyForge
     public static class SkyForgeDefineAssembly
     {
         public const string ASSEMBLY_CONFIG_FILE_NAME = "SkyForgeDefineAssemblyConfig.json";
-        public const string ASSEMBLY_DEFAULT = "Assembly-CSharp";
-
-        private static string m_assemblyName = String.Empty;
+        public static string m_assemblyName = "Assembly-CSharp";
         
         private static Assembly m_assembly;
-        
-#if UNITY_EDITOR
-        public static void SetCustomAssembly(string assemblyName)
-        {
-            m_assemblyName = assemblyName;
-            SessionState.SetString("AssemblyDefine", assemblyName);
-            
-            var assemblyDto = new AssemblyDto() { assemblyName = assemblyName };
-            var jsonText = JsonUtility.ToJson(assemblyDto);
-            
-            var currentDirectory = Directory.GetCurrentDirectory();
-            File.WriteAllText(Path.Combine(currentDirectory, ASSEMBLY_CONFIG_FILE_NAME), jsonText);
-            
-            m_assembly = null;
-        }
 
-        public static void LoadCustomAssembly(AssemblyDto assemblyDto)
-        {
-            m_assemblyName =  assemblyDto.assemblyName;
-            SessionState.SetString("AssemblyDefine", assemblyDto.assemblyName);
-            m_assembly = null;
-        }
-        
-#endif
         public static string GetAssemblyName()
         {
-            if (string.IsNullOrEmpty(m_assemblyName))
-            {
-                m_assemblyName = SessionState.GetString("AssemblyDefine", ASSEMBLY_DEFAULT);
-            }
-            
-            return m_assemblyName;   
+            return m_assemblyName;
         }
         
         public static Assembly GetPlayerAssembly()
