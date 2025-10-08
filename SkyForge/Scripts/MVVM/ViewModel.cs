589ca923b0ed7f023c44af2e39870c7b13ed19f9
tenxdevelop <tenxdeveloper@bk.ru>
2025-10-08 18:06:14 +0500

    Added base ViewModel

---
+++ SkyForge/Scripts/MVVM/ViewModel.cs
@@ -0,0 +1,24 @@
+/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+namespace SkyForge.MVVM
+{
+    public abstract class ViewModel : IViewModel
+    {
+        public virtual void Update(float deltaTime)
+        {
+            
+        }
+
+        public virtual void PhysicsUpdate(float deltaTime)
+        {
+            
+        }
+
+        public virtual void Dispose()
+        {
+            
+        }
+    }
+}
