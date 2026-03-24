55e4b004b8142a5bb1b478719b907c92ab35fd3b
tenxdevelop <tenxdeveloper@bk.ru>
2026-03-24 15:28:39 +0500

    added UIScreenViewToUnityEvent binder

---
+++ SkyForge/Scripts/MVVM/Binders/UnityEventBInders/UIScreenViewCollectionToUnityEventBinder.cs
@@ -0,0 +1,43 @@
+/**************************************************************************\
+   Copyright SkyForge Corporation. All Rights Reserved.
+\**************************************************************************/
+
+using UnityEngine.Events;
+using SkyForge.Extension;
+using SkyForge.Reactive;
+using UnityEngine;
+
+namespace SkyForge.MVVM.Binders
+{
+    public class UIScreenViewCollectionToUnityEventBinder : ObservableBinder<UIScreenView>
+    {
+        
+        [SerializeField] private UnityEvent<UIScreenView> _eventAdded;
+        [SerializeField] private UnityEvent<UIScreenView> _eventRemoved;
+        [SerializeField] private UnityEvent _eventCleared;
+        protected override void OnPropertyChanged(object sender, UIScreenView newValue)
+        {
+            
+        }
+
+        protected override IBinding BindInternal(IViewModel viewModel)
+        {
+            return BindCollection(PropertyName, viewModel, OnAdded, OnRemoved, OnCleared);
+        }
+
+        private void OnAdded(UIScreenView view)
+        {
+            _eventAdded?.Invoke(view);
+        }
+
+        private void OnRemoved(UIScreenView view)
+        {
+            _eventRemoved?.Invoke(view);
+        }
+
+        private void OnCleared()
+        {
+            _eventCleared?.Invoke();   
+        }
+    }
+}
